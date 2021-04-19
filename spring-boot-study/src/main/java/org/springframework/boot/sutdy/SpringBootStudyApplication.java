ea5d4b3b1eef8287a1349c7682e53fbf57ed04b5
nicky <tangjingshan800@163.com>
2021-04-19 14:15:28 +0800

    初始化测试项目

--- spring-boot-study/src/main/java/org/springframework/boot/sutdy/SpringBootStudyApplication.java
+++ spring-boot-study/src/main/java/org/springframework/boot/sutdy/SpringBootStudyApplication.java
@@ -4,7 +4,7 @@ package org.springframework.boot.sutdy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.sutdy.service.TestServiceA;
-import org.springframework.boot.sutdy.utils.MySpringUtils;
+import org.springframework.context.ApplicationContext;
 
 /**
  * 必须要多加一个包，因为默认是扫描当前类所在包下的所有bean
@@ -13,10 +13,11 @@ import org.springframework.boot.sutdy.utils.MySpringUtils;
 public class SpringBootStudyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootStudyApplication.class, args);
-		TestServiceA testServiceA = MySpringUtils.getBeanByClass(TestServiceA.class);
+		ApplicationContext applicationContext = SpringApplication.run(SpringBootStudyApplication.class, args);
+
+		TestServiceA testServiceA = applicationContext.getBean(TestServiceA.class);
 		testServiceA.doSomething("from main");//test check
-		System.out.println(2222);
+		System.out.println("build success!...");
 	}
 
 }
